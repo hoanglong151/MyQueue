92460b329653c861df772e83a880b8f669c349aa
HoangLong <nguyenphamhoanglong2000@gmail.com>
2019-10-28 22:22:24 +0700

    MyQueue

---
+++ MyQueue/MyQueue/MyQueue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQueue
+{
+    public class myQueue
+    {
+        private int top;
+        private int get;
+        private string[] buffer;
+        public myQueue(int size)
+        {
+            buffer = new string[size];
+            top = -1;
+        }
+        public myQueue()
+        {
+            buffer = new string[1000];
+            top = -1;
+        }
+        public void Enqueue(string value)
+        {
+            buffer[top + 1] = value;
+            top++;
+            get = 0;
+        }
+        public string Dequeue()
+        {
+            string value = buffer[get];
+            top--;
+            get++;
+            return value;
+        }
+        public string Peek()
+        {
+            string value = buffer[get];
+            return value;
+        }
+        public int Count()
+        {
+            return top + 1;
+        }
+        public void Clear()
+        {
+            top = -1;
+        }
+    }
+}
---
+++ MyQueue/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyQueue;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            myQueue acc = new myQueue(10);
+        }
+        [TestMethod]
+        public void TestEnqueue()
+        {
+            myQueue S = new myQueue();
+            S.Enqueue("1");
+            S.Enqueue("2");
+            S.Enqueue("3");
+            Assert.AreEqual(3, S.Count());
+        }
+        [TestMethod]
+        public void TestDequeue()
+        {
+            myQueue S = new myQueue();
+            S.Enqueue("1");
+            S.Enqueue("2");
+            S.Enqueue("3");
+            Assert.AreEqual("1", S.Dequeue());
+            Assert.AreEqual("2", S.Dequeue());
+        }
+        [TestMethod]
+        public void TestPeek()
+        {
+            myQueue S = new myQueue();
+            S.Enqueue("1");
+            S.Enqueue("2");
+            S.Enqueue("3");
+            Assert.AreEqual("1", S.Peek());
+            Assert.AreEqual(3, S.Count());
+        }
+        [TestMethod]
+        public void TestClear()
+        {
+            myQueue S = new myQueue();
+            S.Enqueue("1");
+            S.Enqueue("2");
+            S.Enqueue("3");
+            S.Clear();
+            Assert.AreEqual(0, S.Count());
+        }
+    }
+
+}
